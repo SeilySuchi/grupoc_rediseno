16de5aa26e01cc4caba4cafceca7d70cfac270d9
JeancarloPerez <jeancarlo.perez@cemaco.com>
2016-02-10 16:29:29 -0600

    Plugin para login y estilos para el mismo

---
+++ wp-content/themes/grupocemaco/content-login.php
@@ -0,0 +1,28 @@
+<section>
+<div class="row-fluid">
+<div class="span12">
+
+<div class="span6 col-a">
+  <h3>¿AÚN NO TIENES CUENTA <br> EN GRUPOCEMACO.COM?</h3>
+  <p>
+    No hay problema, puedes crear <br> tu cuenta ahora mismo</p>
+  <p>
+    ¡Ven, únete y crece con nosotros!
+  </p>
+  <input type="submit" onClick="window.location='?page_id=68'" value="Crear Cuenta">
+</div>
+<div class="span6 col-b">
+  <h3>YA TENGO CUENTA EN <br>GRUPOCEMACO.COM</h3><br>
+
+  <p>
+    Ingresa para completar o actualizar tu perfil
+  </p>
+
+  <div class="entry-content clearfix">
+	  <?php prana_post_style(); ?>
+  </div>
+</div>
+</div>
+</div>
+
+</section>
